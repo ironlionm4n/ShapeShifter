f0dd56e21dba3351940b0704e56e762c30eeff27
rautry6 <94204738+rautry6@users.noreply.github.com>
2023-08-08 17:16:06 -0400

    Convert To New Input System

--- Assets/Inputs/HareControls.cs
+++ Assets/Inputs/HareControls.cs
@@ -114,6 +114,151 @@ public partial class @HareControls: IInputActionCollection2, IDisposable
                     ""isPartOfComposite"": true
                 }
             ]
+        },
+        {
+            ""name"": ""Shifting"",
+            ""id"": ""6f174cb6-9143-4c98-b6cd-b2bf6f53a26a"",
+            ""actions"": [
+                {
+                    ""name"": ""OpenShiftWheel"",
+                    ""type"": ""Button"",
+                    ""id"": ""b5dba357-a003-479f-9a65-0ac1b4407e4b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""NavigateShiftWheel"",
+                    ""type"": ""Button"",
+                    ""id"": ""01e949c7-88d4-4c65-9bc2-d810945197ba"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""WheelSelection"",
+                    ""type"": ""Button"",
+                    ""id"": ""d26de2e8-4b40-4112-8c9f-c5bde7052739"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""a3e87fb0-2d44-4615-9641-cc850ddd7ddd"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""OpenShiftWheel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9846d140-35b6-4b19-b99d-937fee8918e7"",
+                    ""path"": ""<Gamepad>/dpad/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""OpenShiftWheel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""887a09f6-4c35-45cc-b1a5-d945f11440b8"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""NavigateShiftWheel"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""66aaf17b-d5be-4069-908d-8eeb55495bcc"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""NavigateShiftWheel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""a2355783-e8ef-40ad-b062-0c748637e22c"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""NavigateShiftWheel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Controller"",
+                    ""id"": ""4176df3a-fb24-4c32-b31e-706fcf8291df"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""NavigateShiftWheel"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""fc101e84-d893-4f97-b210-0caf432ffd39"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""NavigateShiftWheel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""fbd47209-bc1d-4540-b1ea-9edd7eb972e1"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""NavigateShiftWheel"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5d4492d7-4a1e-46a8-a015-c3b6dec409c7"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""WheelSelection"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5665492d-e73c-4b9b-8f86-b4f7997b1ec9"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""WheelSelection"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -122,6 +267,11 @@ public partial class @HareControls: IInputActionCollection2, IDisposable
         m_HareMovement = asset.FindActionMap("HareMovement", throwIfNotFound: true);
         m_HareMovement_Movement = m_HareMovement.FindAction("Movement", throwIfNotFound: true);
         m_HareMovement_Jump = m_HareMovement.FindAction("Jump", throwIfNotFound: true);
+        // Shifting
+        m_Shifting = asset.FindActionMap("Shifting", throwIfNotFound: true);
+        m_Shifting_OpenShiftWheel = m_Shifting.FindAction("OpenShiftWheel", throwIfNotFound: true);
+        m_Shifting_NavigateShiftWheel = m_Shifting.FindAction("NavigateShiftWheel", throwIfNotFound: true);
+        m_Shifting_WheelSelection = m_Shifting.FindAction("WheelSelection", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -233,9 +383,77 @@ public partial class @HareControls: IInputActionCollection2, IDisposable
         }
     }
     public HareMovementActions @HareMovement => new HareMovementActions(this);
+
+    // Shifting
+    private readonly InputActionMap m_Shifting;
+    private List<IShiftingActions> m_ShiftingActionsCallbackInterfaces = new List<IShiftingActions>();
+    private readonly InputAction m_Shifting_OpenShiftWheel;
+    private readonly InputAction m_Shifting_NavigateShiftWheel;
+    private readonly InputAction m_Shifting_WheelSelection;
+    public struct ShiftingActions
+    {
+        private @HareControls m_Wrapper;
+        public ShiftingActions(@HareControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @OpenShiftWheel => m_Wrapper.m_Shifting_OpenShiftWheel;
+        public InputAction @NavigateShiftWheel => m_Wrapper.m_Shifting_NavigateShiftWheel;
+        public InputAction @WheelSelection => m_Wrapper.m_Shifting_WheelSelection;
+        public InputActionMap Get() { return m_Wrapper.m_Shifting; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(ShiftingActions set) { return set.Get(); }
+        public void AddCallbacks(IShiftingActions instance)
+        {
+            if (instance == null || m_Wrapper.m_ShiftingActionsCallbackInterfaces.Contains(instance)) return;
+            m_Wrapper.m_ShiftingActionsCallbackInterfaces.Add(instance);
+            @OpenShiftWheel.started += instance.OnOpenShiftWheel;
+            @OpenShiftWheel.performed += instance.OnOpenShiftWheel;
+            @OpenShiftWheel.canceled += instance.OnOpenShiftWheel;
+            @NavigateShiftWheel.started += instance.OnNavigateShiftWheel;
+            @NavigateShiftWheel.performed += instance.OnNavigateShiftWheel;
+            @NavigateShiftWheel.canceled += instance.OnNavigateShiftWheel;
+            @WheelSelection.started += instance.OnWheelSelection;
+            @WheelSelection.performed += instance.OnWheelSelection;
+            @WheelSelection.canceled += instance.OnWheelSelection;
+        }
+
+        private void UnregisterCallbacks(IShiftingActions instance)
+        {
+            @OpenShiftWheel.started -= instance.OnOpenShiftWheel;
+            @OpenShiftWheel.performed -= instance.OnOpenShiftWheel;
+            @OpenShiftWheel.canceled -= instance.OnOpenShiftWheel;
+            @NavigateShiftWheel.started -= instance.OnNavigateShiftWheel;
+            @NavigateShiftWheel.performed -= instance.OnNavigateShiftWheel;
+            @NavigateShiftWheel.canceled -= instance.OnNavigateShiftWheel;
+            @WheelSelection.started -= instance.OnWheelSelection;
+            @WheelSelection.performed -= instance.OnWheelSelection;
+            @WheelSelection.canceled -= instance.OnWheelSelection;
+        }
+
+        public void RemoveCallbacks(IShiftingActions instance)
+        {
+            if (m_Wrapper.m_ShiftingActionsCallbackInterfaces.Remove(instance))
+                UnregisterCallbacks(instance);
+        }
+
+        public void SetCallbacks(IShiftingActions instance)
+        {
+            foreach (var item in m_Wrapper.m_ShiftingActionsCallbackInterfaces)
+                UnregisterCallbacks(item);
+            m_Wrapper.m_ShiftingActionsCallbackInterfaces.Clear();
+            AddCallbacks(instance);
+        }
+    }
+    public ShiftingActions @Shifting => new ShiftingActions(this);
     public interface IHareMovementActions
     {
         void OnMovement(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+    }
+    public interface IShiftingActions
+    {
+        void OnOpenShiftWheel(InputAction.CallbackContext context);
+        void OnNavigateShiftWheel(InputAction.CallbackContext context);
+        void OnWheelSelection(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     UIInvoker _uiInvoker;
+    ShiftInvoker _shiftInvoker;
 
     [SerializeField] private GameObject shiftUI;
     [SerializeField, Tooltip("Clockwise")] private Button[] shiftButtons;
@@ -17,50 +20,76 @@ public class UIManager : MonoBehaviour
     private Vector3 _mousePosition;
     private Camera _mainCamera;
     private Button _selectedButton;
+    private Button _lastSelectedButton;
     private int _selectedButtonIndex;
 
     //Controls when the shift UI is visible
     private bool _shifterActive = false;
 
+    private HareControls _hareActionMap;
+    private InputAction _shiftOpen;
+    private InputAction _navigateShiftWheel;
+    private InputAction _selectShift;
+
+    private void OnEnable()
+    {
+        _hareActionMap = new HareControls();
+        _hareActionMap.Enable();
+
+        _shiftOpen = _hareActionMap.Shifting.OpenShiftWheel;
+        _navigateShiftWheel = _hareActionMap.Shifting.NavigateShiftWheel;
+        _selectShift = _hareActionMap.Shifting.WheelSelection;
+    }
+
+    private void OnDisable()
+    {
+        _hareActionMap.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        _uiInvoker = InvokerHolder.Instance.UIInvoker;   
+        _uiInvoker = InvokerHolder.Instance.UIInvoker;
+        _shiftInvoker = InvokerHolder.Instance.ShiftInvoker;
         _mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (_shiftOpen.WasPressedThisFrame())
         {
             Shift();
         }
 
         if (_shifterActive)
         {
-            if(Input.GetKeyDown(KeyCode.A))
+            if (_navigateShiftWheel.WasPressedThisFrame())
             {
-                _selectedButtonIndex--;
-
-                if( _selectedButtonIndex < 0 )
+                float shiftMovement = _navigateShiftWheel.ReadValue<float>();
+                if (shiftMovement < 0)
                 {
-                    _selectedButtonIndex = shiftButtons.Length - 1;
+                    _selectedButtonIndex--;
+
+                    if (_selectedButtonIndex < 0)
+                    {
+                        _selectedButtonIndex = shiftButtons.Length - 1;
+                    }
+
+                    _selectedButton = shiftButtons[_selectedButtonIndex];
                 }
 
-                _selectedButton = shiftButtons[_selectedButtonIndex];
-            }
-
-            if(Input.GetKeyDown(KeyCode.D))
-            {
-                _selectedButtonIndex++;
-
-                if( _selectedButtonIndex > shiftButtons.Length - 1)
+                if (shiftMovement > 0)
                 {
-                    _selectedButtonIndex = 0;
-                }
+                    _selectedButtonIndex++;
 
-                _selectedButton = shiftButtons[_selectedButtonIndex];
+                    if (_selectedButtonIndex > shiftButtons.Length - 1)
+                    {
+                        _selectedButtonIndex = 0;
+                    }
+
+                    _selectedButton = shiftButtons[_selectedButtonIndex];
+                }
             }
 
             //Points the pointer towards the selected button
@@ -75,6 +104,16 @@ public class UIManager : MonoBehaviour
 
                 float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
                 centerPoint.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle-90));
+
+                if (_selectShift.WasPressedThisFrame())
+                {
+                    _selectedButton.onClick.Invoke();
+                    _selectedButton.interactable = false;
+                    _lastSelectedButton.interactable = true;
+                    _lastSelectedButton = _selectedButton;
+
+                    Shift();
+                }
             }
         }
     }
@@ -95,6 +134,7 @@ public class UIManager : MonoBehaviour
     {
         _selectedButton = button;
         _selectedButtonIndex = Array.IndexOf(shiftButtons, button);
+        _lastSelectedButton = _selectedButton;
     }
 
     private void SetSelectedButtonDefault()
@@ -107,11 +147,38 @@ public class UIManager : MonoBehaviour
             {
                 _selectedButton = shiftButtons[i];
                 _selectedButtonIndex = i;
+
+                if (_lastSelectedButton == null)
+                {
+                    _lastSelectedButton = _selectedButton;
+                }
+
+                _selectedButton.interactable = false;
                 return;
             }
         }
 
+
+
         _selectedButton = null;
         Debug.LogWarning("Warning: No Shift Button matching animal name");
+    }
+
+    public void ShiftToWolf()
+    {
+        IAction newAction = new WolfShiftAction(GameObject.Find("Player"));
+        _shiftInvoker.AddAction(newAction, false);
+    }
+
+    public void ShiftToRaven()
+    {
+        IAction newAction = new RavenShiftAction(GameObject.Find("Player"));
+        _shiftInvoker.AddAction(newAction, false);
+    }
+
+    public void ShiftToRabbit()
+    {
+        IAction newAction = new RabbitShiftAction(GameObject.Find("Player"));
+        _shiftInvoker.AddAction(newAction, false);
     }
 }
